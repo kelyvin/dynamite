cbcb650463583745b3fdf9eed256c50305f24425
Kelvin Nguyen <kelvinnuci@gmail.com>
2019-03-24 17:36:57 -0700

    Add prototype for catch

--- dynamite.js
+++ dynamite.js
@@ -1,3 +1,9 @@
+var Q = require('kew')
+
+Q.Promise.prototype.catch = Q.Promise.prototype.fail
+// This is done to support promse A+ spec compatibility
+// for dynamite promises that are based on kew
+
 module.exports = {
   Client: require('./lib/Client'),
   FakeDynamo: require('./lib/FakeDynamo'),
